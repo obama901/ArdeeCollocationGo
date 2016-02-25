b7d0cec8be22263b9bb9738712cf9da9c8611c86
Ardee <ardee@ardeedemacbook-pro.local>
2016-02-25 23:14:13 +0800

    Initial Commit

---
+++ ArdeeCollocationGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArdeeCollocationGo
+//
+//  Created by Ardee on 16/2/25.
+//  Copyright © 2016年 Ardee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ArdeeCollocationGo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ArdeeCollocationGo
+//
+//  Created by Ardee on 16/2/25.
+//  Copyright © 2016年 Ardee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
